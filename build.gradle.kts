f6253364f93fe194cb38e4bad11a6ce618601da6
omoikane245 <omoikane245@gmail.com>
2024-10-26 23:22:59 +0900

    docker plugin version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
-    id("com.avast.gradle.docker-compose") version "0.14.0"
+    id("com.avast.gradle.docker-compose") version "0.17.10"
     id("io.gitlab.arturbosch.detekt") version "1.17.0"
 }
 
