da54d0375f4475d9ecf129921b92e7a0d44e5df1
omoikane245 <omoikane245@gmail.com>
2024-10-27 21:57:43 +0900

    spring version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.lang.Thread.sleep
 
 plugins {
-    id("org.springframework.boot") version "2.7.7"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     id("com.avast.gradle.docker-compose") version "0.17.10"
@@ -29,11 +29,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.0")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.1")
     implementation("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:1.13.3")
-    testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:2.3.0")
+    testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:2.3.1")
 }
 
 detekt {
