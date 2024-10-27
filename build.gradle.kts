97d2e6c99da542ed311428aeafd998b874780c6d
omoikane245 <omoikane245@gmail.com>
2024-10-28 00:07:36 +0900

    add jacoco plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.lang.Thread.sleep
 
 plugins {
+    jacoco
     id("org.springframework.boot") version "2.7.7"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
